608806711c7d240698d3db2ef54aca957b7394cc
https-richardy <=>
2023-11-25 23:56:21 -0300

    'CategoryService' added

---
+++ Source/Services/CategoryService.cs
@@ -0,0 +1,57 @@
+using OpenMovies.Models;
+using OpenMovies.Repositories;
+
+namespace OpenMovies.Services;
+
+public class CategoryService
+{
+    private readonly CategoryRepository _categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+    public async Task CreateCategory(Category category)
+    {
+        var existingCategory = await _categoryRepository.GetAsync(c => c.Name == category.Name);
+        if (existingCategory != null)
+            throw new InvalidOperationException("A category with the same name already exists.");
+
+        await _categoryRepository.AddAsync(category);
+    }
+
+    public async Task<IEnumerable<Category>> GetAllCategories()
+    {
+        return await _categoryRepository.GetAllAsync();
+    }
+
+    public async Task<Category> GetCategoryById(int id)
+    {
+        var category = await _categoryRepository.GetByIdAsync(id);
+        if (category == null)
+        {
+            throw new InvalidOperationException($"Category with ID {id} not found.");
+        }
+
+        return category;
+    }
+
+    public async Task UpdateCategory(Category category)
+    {
+        var existingCategory = await _categoryRepository.GetByIdAsync(category.Id);
+        if (existingCategory == null)
+            throw new InvalidOperationException("Category not found.");
+
+        await _categoryRepository.UpdateAsync(category);
+    }
+
+    public async Task DeleteCategory(int categoryId)
+    {
+        var category = await _categoryRepository.GetByIdAsync(categoryId);
+        if (category == null)
+            throw new InvalidOperationException("Category not found.");
+
+        await _categoryRepository.DeleteAsync(category);
+    }
+}
