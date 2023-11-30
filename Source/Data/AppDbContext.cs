10f51e0dd2b1f878477d50b1f57fc774144c9af0
https-richardy <=>
2023-11-29 21:23:04 -0300

    renamed: Image to Cover in AppDbContext

--- Source/Data/AppDbContext.cs
+++ Source/Data/AppDbContext.cs
@@ -9,6 +9,7 @@ public class AppDbContext : DbContext
     public DbSet<Category> Categories { get; set; }
     public DbSet<Director> Directors { get; set; }
     public DbSet<Trailer> Trailers { get; set; }
+    public DbSet<Cover> Covers { get; set; }
 
     public AppDbContext(DbContextOptions options)
     : base(options) {  }
