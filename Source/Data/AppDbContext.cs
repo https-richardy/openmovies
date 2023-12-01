0d632f4097abb86a6280d14b261a4526290baa2f
https-richardy <=>
2023-12-01 09:14:56 -0300

    Extend 'AppDbContext' from 'IdentityDbContext' to incorporate Identity framework functionalities for user and role management. This modification allows seamless integration of Identity-related features into the existing DbContext, enhancing the application's authentication and authorization capabilities.

--- Source/Data/AppDbContext.cs
+++ Source/Data/AppDbContext.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using OpenMovies.Models;
 
 namespace OpenMovies.Data;
 
-public class AppDbContext : DbContext
+public class AppDbContext : IdentityDbContext
 {
     public DbSet<Movie> Movies { get; set; }
     public DbSet<Category> Categories { get; set; }
