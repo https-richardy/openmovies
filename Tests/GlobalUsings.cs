32895eec12c7fb2d9fea3485d46aaab3e79820e5
https-richardy <=>
2023-12-05 13:24:21 -0300

    added some global usings

--- Tests/GlobalUsings.cs
+++ Tests/GlobalUsings.cs
@@ -1,6 +1,9 @@
 global using Xunit;
 global using Moq;
+global using Microsoft.EntityFrameworkCore;
 
 global using OpenMovies.Controllers;
 global using OpenMovies.Repositories;
 global using OpenMovies.Services;
+global using OpenMovies.Models;
+global using OpenMovies.Data;
