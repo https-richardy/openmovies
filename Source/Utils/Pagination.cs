61a5fe7922e494287e6f8b1345e9d55819f8b169
https-richardy <=>
2023-12-05 10:22:58 -0300

    #refactor: remove constructor overload accepting a repository from Pagination class

--- Source/Utils/Pagination.cs
+++ Source/Utils/Pagination.cs
@@ -1,6 +1,5 @@
 #pragma warning disable CS8603
 using OpenMovies.Models;
-using OpenMovies.Repositories;
 
 namespace OpenMovies.Utils;
 
@@ -11,20 +10,6 @@ public class Pagination<TEntity>
     public string Next { get; set; }
     public string Previous { get; set; }
     public List<TEntity> Results { get; set; }
-
-    public Pagination(IRepository<TEntity> repository, int pageNumber, int pageSize, HttpContext httpContext)
-    {
-        Count = repository.GetAllAsync().Result.Count();
-
-        int totalPages = (int)Math.Ceiling(Count / (double)pageSize);
-        Next = CalculateNextUrl(httpContext.Request.Path, pageNumber, totalPages);
-        Previous = CalculatePreviousUrl(httpContext.Request.Path, pageNumber);
-
-        Results = repository.GetAllAsync().Result
-            .Skip((pageNumber - 1) * pageSize)
-            .Take(pageSize)
-            .ToList();
-    }
 
     public Pagination(IEnumerable<TEntity> data, int pageNumber, int pageSize, HttpContext httpContext)
     {
