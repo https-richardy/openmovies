cc1ece040034d97004fd8b60a99d3390bbd253f8
https-richardy <=>
2023-12-05 22:09:50 -0300

    added OpenMovies.DTOs global using

--- Tests/GlobalUsings.cs
+++ Tests/GlobalUsings.cs
@@ -7,5 +7,6 @@ global using OpenMovies.Controllers;
 global using OpenMovies.Repositories;
 global using OpenMovies.Services;
 global using OpenMovies.Models;
+global using OpenMovies.DTOs;
 global using OpenMovies.Models.Enums;
 global using OpenMovies.Data;
