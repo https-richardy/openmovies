19abc6bfa4749d2c30c5a00ec10a0e9b538111e8
https-richardy <=>
2023-12-06 18:25:16 -0300

    removed '[FromForm]' data annotation from 'cover' property in MovieDTO

--- Source/Models/DTOs/MovieDTO.cs
+++ Source/Models/DTOs/MovieDTO.cs
@@ -8,7 +8,9 @@ public class MovieDTO
     public DateTime ReleaseDateOf { get; set; }
     public string Synopsis { get; set; } = string.Empty;
 
-    [FromForm] public IFormFile Cover { get; set; }
+    public IFormFile Cover { get; set; }
+
+    [FromForm]
     public List<TrailerDTO>? Trailers { get; set; } = new List<TrailerDTO>();
     public int DirectorId { get; set; }
     public int CategoryId { get; set; }
