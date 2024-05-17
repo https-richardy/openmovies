2cbbeda2ed5ffb03a31b6315321383c1e4c3bd43
https-richardy <code.richardy@gmail.com>
2024-05-17 20:57:41 -0300

    feat: introducing the AddApplicationServices extension method. This extension method is for registering application services in the dependency injection container.

---
+++ Source/Extensions/ApplicationServicesExtension.cs
@@ -0,0 +1,10 @@
+namespace OpenMovies.WebApi.Extensions;
+
+public static class ApplicationServicesExtension
+{
+    public static void AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<IMovieService, MovieService>();
+        services.AddScoped<ICategoryService, CategoryService>();
+    }
+}
