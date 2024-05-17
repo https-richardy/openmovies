5bc59d65a17a3bb4cdb3a5cf987e50ed530b8814
https-richardy <code.richardy@gmail.com>
2024-05-17 20:40:13 -0300

    feat: add data persistence extension for database configuration and repositories

---
+++ Source/Extensions/DataPersistenceExtension.cs
@@ -0,0 +1,16 @@
+namespace OpenMovies.WebApi.Extensions;
+
+public static class DataPersistenceExtension
+{
+    public static void AddDataPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Default");
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlite(connectionString);
+        });
+
+        services.AddScoped<IMovieRepository, MovieRepository>();
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+    }
+}
