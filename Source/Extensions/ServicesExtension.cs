100413e60ad314cc5074f47b14d91a0d8bcc2e65
https-richardy <code.richardy@gmail.com>
2024-05-17 20:43:26 -0300

    feat: add extension method ConfigureServices to ServicesExtension class in the OpenMovies.WebApi.Extensions namespace.

---
+++ Source/Extensions/ServicesExtension.cs
@@ -0,0 +1,9 @@
+namespace OpenMovies.WebApi.Extensions;
+
+public static class ServicesExtension
+{
+    public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDataPersistence(configuration);
+    }
+}
