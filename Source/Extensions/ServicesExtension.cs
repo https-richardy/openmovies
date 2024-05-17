79a944993c34fbbdde647ebe9a6e7f699f2ef083
https-richardy <code.richardy@gmail.com>
2024-05-17 20:48:21 -0300

    feat: enhance the ConfigureServices method within the ServicesExtension class located in the OpenMovies.WebApi.Extensions namespace. This addition includes configuring Identity services using the ConfigureIdentity extension method after setting up data persistence.

--- Source/Extensions/ServicesExtension.cs
+++ Source/Extensions/ServicesExtension.cs
@@ -5,5 +5,6 @@ public static class ServicesExtension
     public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDataPersistence(configuration);
+        services.ConfigureIdentity();
     }
 }
