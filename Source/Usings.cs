40f9787f97b32ae5569b43f47bb69c7c1d34b9d7
https-richardy <code.richardy@gmail.com>
2024-05-17 20:46:11 -0300

    feat: add global usage directive for Microsoft.AspNetCore.Identity

--- Source/Usings.cs
+++ Source/Usings.cs
@@ -1,5 +1,6 @@
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Identity;
 
 global using OpenMovies.WebApi.Models;
 global using OpenMovies.WebApi.Models.InputModels;
