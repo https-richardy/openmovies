11ded80c4800d92fb93cb29cb3c32ec476108354
https-richardy <code.richardy@gmail.com>
2024-05-17 20:30:51 -0300

    feat: global usage directive for OpenMovies.WebApi.Services was added.

--- Source/Usings.cs
+++ Source/Usings.cs
@@ -5,3 +5,4 @@ global using OpenMovies.WebApi.Models.InputModels;
 global using OpenMovies.WebApi.Data;
 global using OpenMovies.WebApi.Data.Repositories;
 global using OpenMovies.WebApi.Extensions;
+global using OpenMovies.WebApi.Services;
