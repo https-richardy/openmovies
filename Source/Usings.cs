4fc8985a1c5d583f44fe9c4fbe23a663a47e8a2f
https-richardy <code.richardy@gmail.com>
2024-05-17 20:28:19 -0300

    feat: updated the global usings file to import some application namespaces

--- Source/Usings.cs
+++ Source/Usings.cs
@@ -2,3 +2,6 @@ global using Microsoft.AspNetCore.Mvc;
 
 global using OpenMovies.WebApi.Models;
 global using OpenMovies.WebApi.Models.InputModels;
+global using OpenMovies.WebApi.Data;
+global using OpenMovies.WebApi.Data.Repositories;
+global using OpenMovies.WebApi.Extensions;
