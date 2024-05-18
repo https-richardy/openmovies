f20c3fb582cb03eb9c844b2b81b3b6527486b874
https-richardy <code.richardy@gmail.com>
2024-05-17 22:00:43 -0300

    refactor: refactoring the MovieCreationRequest class in the OpenMovies.WebApi.Models.InputModels namespace to a record type. This change improves immutability.

--- Source/Models/InputModels/MovieCreationRequest.cs
+++ Source/Models/InputModels/MovieCreationRequest.cs
@@ -1,6 +1,6 @@
 namespace OpenMovies.WebApi.Models.InputModels;
 
-public sealed class MovieCreationRequest
+public sealed record MovieCreationRequest
 {
     public string Title { get; set; }
     public string Synopsis { get; set; }
