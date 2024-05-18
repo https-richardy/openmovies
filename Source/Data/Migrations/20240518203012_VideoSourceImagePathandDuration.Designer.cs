cd669f65fda00015d26d5b1d549e45d95b3e182f
https-richardy <code.richardy@gmail.com>
2024-05-18 17:30:58 -0300

    feat: added database migration files to reflect latest changes.

--- Source/Data/Migrations/20240518203012_VideoSourceImagePathandDuration.Designer.cs
+++ Source/Data/Migrations/20240518203012_VideoSourceImagePathandDuration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OpenMovies.WebApi.Data;
 
@@ -10,9 +11,11 @@ using OpenMovies.WebApi.Data;
 namespace OpenMovies.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240518203012_VideoSourceImagePathandDuration")]
+    partial class VideoSourceImagePathandDuration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.0");
---
+++ Source/Data/Migrations/20240518203012_VideoSourceImagePathandDuration.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OpenMovies.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class VideoSourceImagePathandDuration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_Directors_DirectorId",
+                table: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "Directors");
+
+            migrationBuilder.DropTable(
+                name: "Trailers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Movies_DirectorId",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "DirectorId",
+                table: "Movies");
+
+            migrationBuilder.RenameColumn(
+                name: "ReleaseDateOf",
+                table: "Movies",
+                newName: "VideoSource");
+
+            migrationBuilder.RenameColumn(
+                name: "CoverImagePath",
+                table: "Movies",
+                newName: "ImagePath");
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "Duration",
+                table: "Movies",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Movies");
+
+            migrationBuilder.RenameColumn(
+                name: "VideoSource",
+                table: "Movies",
+                newName: "ReleaseDateOf");
+
+            migrationBuilder.RenameColumn(
+                name: "ImagePath",
+                table: "Movies",
+                newName: "CoverImagePath");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DirectorId",
+                table: "Movies",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Directors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Directors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trailers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    MovieId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Link = table.Column<string>(type: "TEXT", nullable: false),
+                    Plataform = table.Column<int>(type: "INTEGER", nullable: false),
+                    Type = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trailers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Trailers_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_DirectorId",
+                table: "Movies",
+                column: "DirectorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Trailers_MovieId",
+                table: "Trailers",
+                column: "MovieId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_Directors_DirectorId",
+                table: "Movies",
+                column: "DirectorId",
+                principalTable: "Directors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
