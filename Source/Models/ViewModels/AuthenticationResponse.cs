679bd7a57c110c2f21066852e5011b4bbaedb761
https-richardy <code.richardy@gmail.com>
2024-05-18 14:01:21 -0300

    feat: added additional properties to the AuthenticationResponse record including Message and Success to provide more information about the authentication process. Also, added static methods FailureResponse() and SuccessResponse() to generate failure and success responses respectively, with optional custom messages.

--- Source/Models/ViewModels/AuthenticationResponse.cs
+++ Source/Models/ViewModels/AuthenticationResponse.cs
@@ -3,4 +3,25 @@ namespace OpenMovies.WebApi.Models.ViewModels;
 public record AuthenticationResponse
 {
     public string Token { get; set; }
+    public string Message { get; set; }
+    public bool Success { get; set; }
+
+    public static AuthenticationResponse FailureResponse() => new AuthenticationResponse
+    {
+        Success = false,
+        Message = "invalid credentials."
+    };
+
+    public static AuthenticationResponse FailureResponse(string message) => new AuthenticationResponse
+    {
+        Success = false,
+        Message = message
+    };
+
+    public static AuthenticationResponse SuccessResponse(string token) => new AuthenticationResponse
+    {
+        Success = true,
+        Token = token,
+        Message = "Authentication successful."
+    };
 }
