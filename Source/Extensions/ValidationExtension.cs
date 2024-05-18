2eee009d5eada65b83e94eda9ae875db8bb4ba4d
https-richardy <code.richardy@gmail.com>
2024-05-18 15:29:34 -0300

    feat: created extension method to add validations. This extension class will be used to register validation classes in the DI container.

---
+++ Source/Extensions/ValidationExtension.cs
@@ -0,0 +1,9 @@
+namespace OpenMovies.WebApi.Extensions;
+
+public static class ValidationExtension
+{
+    public static void AddValidation(this IServiceCollection services)
+    {
+        services.AddScoped<IValidator<AccountRegistrationRequest>, AccountRegistrationValidator>();
+    }
+}
