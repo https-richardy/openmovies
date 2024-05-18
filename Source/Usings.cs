689b694e6c038744b77310ab8b5b0e1e04bb4aae
https-richardy <code.richardy@gmail.com>
2024-05-18 15:37:34 -0300

    feat: added global usage directive for the OpenMovies.WebApi.Operations.Commands.Handlers namespace

--- Source/Usings.cs
+++ Source/Usings.cs
@@ -24,6 +24,7 @@ global using OpenMovies.WebApi.Data.Repositories;
 global using OpenMovies.WebApi.Extensions;
 global using OpenMovies.WebApi.Services;
 global using OpenMovies.WebApi.Services.Exceptions;
+global using OpenMovies.WebApi.Operations.Commands.Handlers;
 global using OpenMovies.WebApi.Identity;
 global using OpenMovies.WebApi.Utils;
 global using OpenMovies.WebApi.Validators;
