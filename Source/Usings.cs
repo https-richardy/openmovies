1252109de881f99c6509c6e58ffd36214faefe54
https-richardy <code.richardy@gmail.com>
2024-05-17 23:06:08 -0300

    feat: added global usage directive for Microsoft.AspNetCore.Identity.EntityFrameworkCore namespace

--- Source/Usings.cs
+++ Source/Usings.cs
@@ -8,6 +8,7 @@ global using System.Security.Claims;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.IdentityModel.Tokens;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 global using Microsoft.AspNetCore.Identity;
 
 global using OpenMovies.WebApi.Models;
