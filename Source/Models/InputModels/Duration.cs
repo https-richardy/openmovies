2bcfd42be8b02793993add3a3a07b24aee566911
https-richardy <code.richardy@gmail.com>
2024-05-19 15:50:28 -0300

    feat: added an input model to represent the length of a movie.

---
+++ Source/Models/InputModels/Duration.cs
@@ -0,0 +1,8 @@
+namespace OpenMovies.WebApi.Models.InputModels;
+
+public sealed class Duration
+{
+    public int Hours { get; set; }
+    public int Minutes { get; set;}
+    public int Seconds { get; set; }
+}
