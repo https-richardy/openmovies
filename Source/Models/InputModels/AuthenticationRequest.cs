ea109a6f2755063dbf03ee5d9602efba4cfed3dc
https-richardy <code.richardy@gmail.com>
2024-05-19 16:02:57 -0300

    feat: AuthenticationRequest now implements IRequest interface, to work with MediatR

--- Source/Models/InputModels/AuthenticationRequest.cs
+++ Source/Models/InputModels/AuthenticationRequest.cs
@@ -1,6 +1,6 @@
 namespace OpenMovies.WebApi.Models.InputModels;
 
-public record AuthenticationRequest
+public record AuthenticationRequest : IRequest<AuthenticationResponse>
 {
     public string Email { get; set; }
     public string Password { get; set; }
