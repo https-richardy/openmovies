288ce8afc2ca5eba32dde737c90e099b8a797d41
https-richardy <code.richardy@gmail.com>
2024-05-19 16:35:30 -0300

    feat: category service has been removed from the services collection.

--- Source/Extensions/ApplicationServicesExtension.cs
+++ Source/Extensions/ApplicationServicesExtension.cs
@@ -5,8 +5,6 @@ public static class ApplicationServicesExtension
     public static void AddApplicationServices(this IServiceCollection services)
     {
         services.AddScoped<IMovieService, MovieService>();
-        services.AddScoped<ICategoryService, CategoryService>();
-
         services.AddScoped<IAccountService, AccountService>();
 
         services.AddFileUploadService();
