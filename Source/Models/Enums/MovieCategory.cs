3e492c7b7ef8a3ce13460153ba78ed818185f263
https-richardy <code.richardy@gmail.com>
2024-05-19 16:27:17 -0300

    feat: added "MovieCategory" enumerator to represent movie categories.

---
+++ Source/Models/Enums/MovieCategory.cs
@@ -0,0 +1,27 @@
+namespace OpenMovies.WebApi.Models.Enums;
+
+public enum MovieCategory
+{
+    Action,
+    Adventure,
+    Animation,
+    Biography,
+    Comedy,
+    Crime,
+    Documentary,
+    Drama,
+    Family,
+    Fantasy,
+    History,
+    Horror,
+    Musical,
+    Mystery,
+    Romance,
+    SciFi,
+    Short,
+    Sport,
+    Thriller,
+    War,
+    Western,
+    Other
+}
