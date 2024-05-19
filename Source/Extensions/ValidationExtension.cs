1f0c7631a5927e31f9e920a3a86a6be9aaa256d3
https-richardy <code.richardy@gmail.com>
2024-05-19 16:12:35 -0300

    feat: authentication request validator was registered in the service collection.

--- Source/Extensions/ValidationExtension.cs
+++ Source/Extensions/ValidationExtension.cs
@@ -5,5 +5,6 @@ public static class ValidationExtension
     public static void AddValidation(this IServiceCollection services)
     {
         services.AddScoped<IValidator<AccountRegistrationRequest>, AccountRegistrationValidator>();
+        services.AddScoped<IValidator<AuthenticationRequest>, AuthenticationRequestValidator>();
     }
 }
