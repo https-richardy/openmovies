a4c7d1575fed69383ac6b33760599fdec21a217b
https-richardy <code.richardy@gmail.com>
2024-05-19 16:36:10 -0300

    feat: update in the mapping of an UpdateMovieRequest to a Movie.

--- Source/Extensions/MappingExtension.cs
+++ Source/Extensions/MappingExtension.cs
@@ -18,6 +18,8 @@ public static class MappingExtension
             config.Bind(source => source.Title, target => target.Title);
             config.Bind(source => source.Synopsis, target => target.Synopsis);
             config.Bind(source => source.ReleaseYear, target => target.ReleaseYear);
+            config.Bind(source => source.VideoSource, target => target.VideoSource);
+            config.Bind(source => source.Duration, target => target.Duration);
         });
 
         TinyMapper.Bind<AccountRegistrationRequest, IdentityUser>(config =>
