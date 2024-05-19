150fd16365df6cff7595f185ca7f09a7957226c6
https-richardy <code.richardy@gmail.com>
2024-05-19 16:31:39 -0300

    feat: change in the MovieCreationRequest input model to reflect the latest changes in the project where the Category entity was removed, therefore the "Category" (class) navigation property was changed to the MovieCategory Enum.

--- Source/Models/InputModels/MovieCreationRequest.cs
+++ Source/Models/InputModels/MovieCreationRequest.cs
@@ -5,10 +5,9 @@ public sealed record MovieCreationRequest
     public string Title { get; set; }
     public string Synopsis { get; set; }
     public string VideoSource { get; set; }
-
     public int ReleaseYear { get; set; }
-    public int CategoryId { get; set; }
 
+    public MovieCategory Category { get; set; }
     public IFormFile Cover { get; set; }
     public Duration Duration { get; set; }
 }
