da5fce35bd5d9cae198498d005624d588018ffc4
https-richardy <code.richardy@gmail.com>
2024-05-20 08:11:15 -0300

    feat: Category entity DbSet has been removed

--- Source/Data/AppDbContext.cs
+++ Source/Data/AppDbContext.cs
@@ -3,7 +3,6 @@ namespace OpenMovies.WebApi.Data;
 public class AppDbContext : IdentityDbContext
 {
     public DbSet<Movie> Movies { get; set; }
-    public DbSet<Category> Categories { get; set; }
 
     public AppDbContext(DbContextOptions options) : base(options)
     {
