3bd5b5bbae4f7cf89ca93cf6621299558a362863
https-richardy <code.richardy@gmail.com>
2024-05-22 22:24:11 -0300

    view model for deleting a movie has been created.

---
+++ Source/Models/ViewModels/MovieDeletionResponse.cs
@@ -0,0 +1,20 @@
+namespace OpenMovies.WebApi.Models.ViewModels;
+
+public sealed class MovieDeletionResponse
+{
+    public string Message { get; set; }
+
+    public bool Success { get; set; }
+
+    public static MovieDeletionResponse SuccessResponse() => new MovieDeletionResponse
+    {
+        Success = true,
+        Message = "Movie created successfully."
+    };
+
+    public static MovieDeletionResponse FailureResponse(string message) => new MovieDeletionResponse
+    {
+        Success = false,
+        Message = message
+    };
+}
