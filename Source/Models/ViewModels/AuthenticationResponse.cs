5252345c20bb867be7fc5a88fb018219d364e03f
https-richardy <code.richardy@gmail.com>
2024-05-27 22:16:24 -0300

    SuccessResponse method with signature accepting a token has been added.

--- Source/Models/ViewModels/AuthenticationResponse.cs
+++ Source/Models/ViewModels/AuthenticationResponse.cs
@@ -4,6 +4,12 @@ public record AuthenticationResponse : OperationResult
 {
     public string Token { get; set; }
 
+    public static new AuthenticationResponse SuccessResponse(string token) => new AuthenticationResponse
+    {
+        Success = true,
+        Token = token
+    };
+
     public static AuthenticationResponse InvalidCredentialsResponse() => new AuthenticationResponse
     {
         Success = false,
