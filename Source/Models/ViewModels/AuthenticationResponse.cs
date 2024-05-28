3da1c6062ae9f430926c09538944b3ab98fa3cc3
https-richardy <code.richardy@gmail.com>
2024-05-27 22:14:42 -0300

    inherit AuthenticationResponse from OperationResult and remove redundant methods

--- Source/Models/ViewModels/AuthenticationResponse.cs
+++ Source/Models/ViewModels/AuthenticationResponse.cs
@@ -1,27 +1,12 @@
 namespace OpenMovies.WebApi.Models.ViewModels;
 
-public record AuthenticationResponse
+public record AuthenticationResponse : OperationResult
 {
     public string Token { get; set; }
-    public string Message { get; set; }
-    public bool Success { get; set; }
 
-    public static AuthenticationResponse FailureResponse() => new AuthenticationResponse
+    public static AuthenticationResponse InvalidCredentialsResponse() => new AuthenticationResponse
     {
         Success = false,
         Message = "invalid credentials."
-    };
-
-    public static AuthenticationResponse FailureResponse(string message) => new AuthenticationResponse
-    {
-        Success = false,
-        Message = message
-    };
-
-    public static AuthenticationResponse SuccessResponse(string token) => new AuthenticationResponse
-    {
-        Success = true,
-        Token = token,
-        Message = "Authentication successful."
     };
 }
