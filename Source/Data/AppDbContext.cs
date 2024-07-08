a4df2da5cb12fcb749b23ffc96bed7d0260cf610
https-richardy <code.richardy@gmail.com>
2024-07-08 02:56:10 -0300

    removed repositories and DbSet from Movie as it has not been modeled yet.

--- Source/Data/AppDbContext.cs
+++ Source/Data/AppDbContext.cs
@@ -2,8 +2,6 @@ namespace OpenMovies.WebApi.Data;
 
 public class AppDbContext : IdentityDbContext
 {
-    public DbSet<Movie> Movies { get; set; }
-
     public AppDbContext(DbContextOptions options) : base(options)
     {
         
