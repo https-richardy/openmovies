f925ec7520ad48bb3b3397e30fdde63803186888
https-richardy <code.richardy@gmail.com>
2024-07-08 11:56:06 -0300

    added Episode entity mapping settings.

---
+++ Source/Data/EntitiesConfiguration/EpisodeEntityConfiguration.cs
@@ -0,0 +1,32 @@
+namespace OpenMovies.WebApi.Data.Configuration;
+
+public sealed class EpisodeEntityConfiguration : IEntityTypeConfiguration<Episode>
+{
+    public void Configure(EntityTypeBuilder<Episode> builder)
+    {
+        builder.ToTable("Episodes");
+        builder.HasKey(episode => episode.Id);
+
+        builder.Property(episode => episode.Title)
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.Property(episode => episode.Synopsis)
+            .HasMaxLength(1000)
+            .IsRequired();
+
+        builder.Property(episode => episode.VideoSource)
+            .IsRequired();
+
+        builder.Property(episode => episode.Number)
+            .IsRequired();
+
+        builder.Property(episode => episode.SeasonNumber)
+            .IsRequired();
+
+        builder.HasOne(episode => episode.Media)
+            .WithMany()
+            .HasForeignKey(episode => episode.Media.Id)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
