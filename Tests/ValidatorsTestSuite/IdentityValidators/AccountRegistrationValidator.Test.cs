a2bea97467baf48cf5a949a901a8fb262f542dc5
https-richardy <code.richardy@gmail.com>
2024-07-08 14:12:11 -0300

    added test scenarios for when the name is empty or shorter than 3 characters.

--- Tests/ValidatorsTestSuite/IdentityValidators/AccountRegistrationValidator.Test.cs
+++ Tests/ValidatorsTestSuite/IdentityValidators/AccountRegistrationValidator.Test.cs
@@ -9,6 +9,38 @@ public sealed class AccountRegistrationValidatorTest
         _validator = new AccountRegistrationValidator();
     }
 
+    [Fact(DisplayName = "Given an empty full name, should have validation error")]
+    public async Task GivenEmptyFullName_ShouldHaveValidationError()
+    {
+        var payload = new AccountRegistrationRequest
+        {
+            FullName = string.Empty
+        };
+
+        var result = await _validator.ValidateAsync(payload);
+        var errorMessages = result.Errors.Select(error => error.ErrorMessage);
+
+        Assert.NotNull(result);
+        Assert.False(result.IsValid);
+        Assert.Contains("Full name is required.", errorMessages);
+    }
+
+    [Fact(DisplayName = "Given a full name with less than 3 characters, should have validation error")]
+    public async Task GivenFullNameWithLessThan3Characters_ShouldHaveValidationError()
+    {
+        var payload = new AccountRegistrationRequest
+        {
+            FullName = "ab"
+        };
+
+        var result = await _validator.ValidateAsync(payload);
+        var errorMessages = result.Errors.Select(error => error.ErrorMessage);
+
+        Assert.NotNull(result);
+        Assert.False(result.IsValid);
+        Assert.Contains("Full name must be at least 3 characters.", errorMessages);
+    }
+
     [Fact(DisplayName = "Given an empty email, should have validation error")]
     public async Task GivenEmptyEmail_ShouldHaveValidationError()
     {
