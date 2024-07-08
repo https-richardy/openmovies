26e7b92cbd6125029a2a0526ab4f231f28be47a2
https-richardy <code.richardy@gmail.com>
2024-07-08 03:06:41 -0300

    removed all handler.

--- Source/Extensions/MediatorExtension.cs
+++ Source/Extensions/MediatorExtension.cs
@@ -8,11 +8,5 @@ public static class MediatorExtension
         {
             configuration.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly());
         });
-
-        services.AddScoped<IRequestHandler<AccountRegistrationRequest, OperationResult>, AccountRegistrationHandler>();
-        services.AddScoped<IRequestHandler<AuthenticationRequest, AuthenticationResponse>, AuthenticationRequestHandler>();
-
-        services.AddScoped<IRequestHandler<MovieCreationRequest, OperationResult>, MovieCreationHandler>();
-        services.AddScoped<IRequestHandler<MovieDeletionRequest, OperationResult>, MovieDeletionHandler>();
     }
 }
