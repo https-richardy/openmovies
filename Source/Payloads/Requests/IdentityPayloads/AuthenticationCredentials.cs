6601dd5e943e8e9a1a8ecfd5440f83c4698a4fa4
https-richardy <code.richardy@gmail.com>
2024-07-08 16:59:46 -0300

    authentication credentials registry updated to support request handling with mediatR

--- Source/Payloads/Requests/IdentityPayloads/AuthenticationCredentials.cs
+++ Source/Payloads/Requests/IdentityPayloads/AuthenticationCredentials.cs
@@ -1,6 +1,6 @@
 namespace OpenMovies.WebApi.Payloads;
 
-public sealed record AuthenticationCredentials
+public sealed record AuthenticationCredentials : IRequest<Response<AuthenticationResponse>>
 {
     public string Email { get; init; }
     public string Password { get; init; }
