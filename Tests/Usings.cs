ebf4a2e13de56a560f3b1290e7c0bf50f1174f75
https-richardy <code.richardy@gmail.com>
2024-07-08 13:47:23 -0300

    added global usage directives for the Moq and Autofixture namespace.

--- Tests/Usings.cs
+++ Tests/Usings.cs
@@ -6,18 +6,10 @@ global using System.Text;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
 global using System.Linq.Expressions;
-global using System.IdentityModel.Tokens.Jwt;
 global using System.Security.Claims;
 
 /* add global usage directives for the Microsoft namespaces here */
 
-global using Microsoft.AspNetCore.Mvc;
-global using Microsoft.AspNetCore.Authentication.JwtBearer;
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.EntityFrameworkCore.Metadata.Builders;
-global using Microsoft.IdentityModel.Tokens;
-global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-global using Microsoft.AspNetCore.Identity;
 
 /* add global usage directives for application namespaces here */
 
@@ -31,6 +23,6 @@ global using OpenMovies.WebApi.Validators;
 global using OpenMovies.WebApi.Middlewares;
 
 /* add global usage directives for third-party namespaces here */
-global using Nelibur.ObjectMapper;
-global using FluentValidation;
-global using MediatR;
+
+global using Moq;
+global using AutoFixture;
