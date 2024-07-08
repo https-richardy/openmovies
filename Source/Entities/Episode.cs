bbb33f25ccf68ae27c4530f263ab8ba4ccd9bef9
https-richardy <code.richardy@gmail.com>
2024-07-08 11:28:27 -0300

    added entity to represent a episode.

---
+++ Source/Entities/Episode.cs
@@ -0,0 +1,19 @@
+namespace OpenMovies.WebApi.Entities;
+
+public sealed class Episode : Entity
+{
+    public string Title { get; set; }
+    public string Synopsis { get; set; }
+    public string VideoSource { get; set; }
+    public int Number { get; set; }
+    public int SeasonNumber { get; set; }
+    public Media Media { get; set; }
+
+    public Episode()
+    {
+        /*
+            Default parameterless constructor included due to Entity Framework Core not setting navigation properties
+            when using constructors. For more information, see: https://learn.microsoft.com/pt-br/ef/core/modeling/constructors
+        */
+    }
+}
