64c3d40cc5cf51bd9bf828de305d2a7a979bf0bf
https-richardy <code.richardy@gmail.com>
2024-07-08 11:26:31 -0300

    adding abstract class to represent a media.

---
+++ Source/Entities/Media.cs
@@ -0,0 +1,17 @@
+namespace OpenMovies.WebApi.Entities;
+
+public abstract class Media : Entity
+{
+    public string Title { get; set; }
+    public string Synopsis { get; set; }
+    public string ImageUrl { get; set; }
+    public Category Category { get; set; }
+
+    public Media()
+    {
+        /*
+            Default parameterless constructor included due to Entity Framework Core not setting navigation properties
+            when using constructors. For more information, see: https://learn.microsoft.com/pt-br/ef/core/modeling/constructors
+        */
+    }
+}
