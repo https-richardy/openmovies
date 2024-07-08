69a3e4325a3855e794295219015ba4d7cb4862ed
https-richardy <code.richardy@gmail.com>
2024-07-08 03:06:03 -0300

    removed all mapping settings.

--- Source/Extensions/MappingExtension.cs
+++ Source/Extensions/MappingExtension.cs
@@ -4,33 +4,6 @@ public static class MappingExtension
 {
     public static void AddMapping(this IServiceCollection services)
     {
-        TinyMapper.Bind<MovieCreationRequest, Movie>(config =>
-        {
-            config.Bind(source => source.Title, target => target.Title);
-            config.Bind(source => source.Synopsis, target => target.Synopsis);
-            config.Bind(source => source.ReleaseYear, target => target.ReleaseYear);
-            config.Bind(source => source.VideoSource, target => target.VideoSource);
-            config.Bind(source => source.Duration, target => target.Duration);
-        });
 
-        TinyMapper.Bind<UpdateMovieRequest, Movie>(config =>
-        {
-            config.Bind(source => source.Title, target => target.Title);
-            config.Bind(source => source.Synopsis, target => target.Synopsis);
-            config.Bind(source => source.ReleaseYear, target => target.ReleaseYear);
-            config.Bind(source => source.VideoSource, target => target.VideoSource);
-            config.Bind(source => source.Duration, target => target.Duration);
-        });
-
-        TinyMapper.Bind<AccountRegistrationRequest, IdentityUser>(config =>
-        {
-            config.Bind(source => source.UserName, target => target.UserName);
-            config.Bind(source => source.Email, target => target.Email);
-        });
-
-        TinyMapper.Bind<AuthenticationRequest, IdentityUser>(config =>
-        {
-            config.Bind(source => source.Email, target => target.Email);
-        });
     }
 }
