e74d72f7953908b8688f24f9afc064ad907353d2
https-richardy <code.richardy@gmail.com>
2024-07-08 16:05:25 -0300

    added global using directive for the using OpenMovies.WebApi.Handlers namespace

--- Source/Usings.cs
+++ Source/Usings.cs
@@ -27,6 +27,7 @@ global using OpenMovies.WebApi.Entities;
 global using OpenMovies.WebApi.Services;
 global using OpenMovies.WebApi.Services.Exceptions;
 global using OpenMovies.WebApi.Payloads;
+global using OpenMovies.WebApi.Handlers;
 global using OpenMovies.WebApi.Validators;
 global using OpenMovies.WebApi.Middlewares;
 
