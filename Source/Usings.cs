e01edc99f73d088509e500220e9157d38b44d684
https-richardy <code.richardy@gmail.com>
2024-07-08 11:36:31 -0300

    added new global usage directive for OpenMovies.WebApi.Entities namespace.

--- Source/Usings.cs
+++ Source/Usings.cs
@@ -22,6 +22,7 @@ global using Microsoft.AspNetCore.Identity;
 
 global using OpenMovies.WebApi.Data;
 global using OpenMovies.WebApi.Extensions;
+global using OpenMovies.WebApi.Entities;
 global using OpenMovies.WebApi.Services;
 global using OpenMovies.WebApi.Services.Exceptions;
 global using OpenMovies.WebApi.Validators;
