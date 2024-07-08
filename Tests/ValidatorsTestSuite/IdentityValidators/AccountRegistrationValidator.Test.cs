aa4eb5d2334e976cfa5e45baead255d8dd09060a
https-richardy <code.richardy@gmail.com>
2024-07-08 14:15:18 -0300

    added test scenarios for the case where the password is invalid from the required length to the required format.

--- Tests/ValidatorsTestSuite/IdentityValidators/AccountRegistrationValidator.Test.cs
+++ Tests/ValidatorsTestSuite/IdentityValidators/AccountRegistrationValidator.Test.cs
@@ -75,4 +75,56 @@ public sealed class AccountRegistrationValidatorTest
         Assert.False(result.IsValid);
         Assert.Contains("Invalid email address format.", errorMessages);
     }
+
+    [Fact(DisplayName = "Given an empty password, should have validation error")]
+    public async Task GivenEmptyPassword_ShouldHaveValidationError()
+    {
+        var payload = new AccountRegistrationRequest
+        {
+            Password = string.Empty
+        };
+
+        var result = await _validator.ValidateAsync(payload);
+        var errorMessages = result.Errors.Select(error => error.ErrorMessage);
+
+        Assert.NotNull(result);
+        Assert.False(result.IsValid);
+        Assert.Contains("Password is required.", errorMessages);
+    }
+
+    [Fact(DisplayName = "Given a password with less than 8 characters, should have validation error")]
+    public async Task GivenPasswordWithLessThan8Characters_ShouldHaveValidationError()
+    {
+        var payload = new AccountRegistrationRequest
+        {
+            Password = "1234567"
+        };
+
+        var result = await _validator.ValidateAsync(payload);
+        var errorMessages = result.Errors.Select(error => error.ErrorMessage);
+
+        Assert.NotNull(result);
+        Assert.False(result.IsValid);
+        Assert.Contains("Password must be at least 8 characters.", errorMessages);
+    }
+
+    [Theory(DisplayName = "Given an invalid password format, should have validation error")]
+    [InlineData("password")]
+    [InlineData("password123")]
+    [InlineData("12345678")]
+    [InlineData("Password123")]
+    public async Task GivenInvalidPasswordFormat_ShouldHaveValidationError(string password)
+    {
+        var payload = new AccountRegistrationRequest
+        {
+            Password = password
+        };
+
+        var result = await _validator.ValidateAsync(payload);
+        var errorMessages = result.Errors.Select(error => error.ErrorMessage);
+
+        Assert.NotNull(result);
+        Assert.False(result.IsValid);
+        Assert.Contains("Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character.", errorMessages);
+    }
 }
