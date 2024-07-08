4dc822c6dcef7c5b499aa7e6b82898dc774241b0
https-richardy <code.richardy@gmail.com>
2024-07-08 03:07:07 -0300

    removed all validators.

--- Source/Extensions/ValidationExtension.cs
+++ Source/Extensions/ValidationExtension.cs
@@ -4,9 +4,5 @@ public static class ValidationExtension
 {
     public static void AddValidation(this IServiceCollection services)
     {
-        services.AddScoped<IValidator<AccountRegistrationRequest>, AccountRegistrationValidator>();
-        services.AddScoped<IValidator<AuthenticationRequest>, AuthenticationRequestValidator>();
-
-        services.AddScoped<IValidator<Movie>, MovieValidator>();
     }
 }
