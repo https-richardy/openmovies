42887995406789674383e9ab70194ec73c9a3ff2
https-richardy <code.richardy@gmail.com>
2024-07-08 22:38:47 -0300

    added unit test to verify if the mapping is correct.

--- Source/Extensions/MappingExtension.cs
+++ Source/Extensions/MappingExtension.cs
@@ -4,6 +4,17 @@ public static class MappingExtension
 {
     public static void AddMapping(this IServiceCollection services)
     {
+        #region mapping settings for movies
 
+        TinyMapper.Bind<MovieCreationRequest, Movie>(config =>
+        {
+            config.Bind(target: target => target.Title, source: source => source.Title);
+            config.Bind(target: target => target.Synopsis, source: source => source.Synopsis);
+            config.Bind(target: target => target.VideoSource, source: source => source.VideoSource);
+            config.Bind(target: target => target.ReleaseYear, source: source => source.ReleaseYear);
+            config.Bind(target: target => target.DurationInMinutes, source: source => source.DurationInMinutes);
+        });
+
+        #endregion
     }
 }
