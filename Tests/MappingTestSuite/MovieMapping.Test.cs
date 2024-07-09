1925cac01f0a7b4840baa7a28d2ad38244ad44bf
https-richardy <code.richardy@gmail.com>
2024-07-09 12:39:34 -0300

    added test to verify that the mapping between a movie update request and a movie entity is being mapped correctly.

--- Tests/MappingTestSuite/MovieMapping.Test.cs
+++ Tests/MappingTestSuite/MovieMapping.Test.cs
@@ -35,4 +35,27 @@ public sealed class MovieMappingTest
         Assert.Equal(movieCreationRequest.ReleaseYear, mappedMovie.ReleaseYear);
         Assert.Equal(movieCreationRequest.DurationInMinutes, mappedMovie.DurationInMinutes);
     }
+
+    [Fact(DisplayName = "Should map a MovieUpdateRequest to a Movie entity correctly.")]
+    public void ShouldMapAMovieUpdateRequestToAMovieEntityCorrectly()
+    {
+        var movieUpdateRequest = new MovieUpdateRequest
+        {
+            Title = "Title",
+            Synopsis = "Synopsis",
+            VideoSource = "https://www.youtube.com/watch?v=1234",
+            ReleaseYear = 2022,
+            DurationInMinutes = 120,
+            CategoryId = 1,
+            MovieId = 1
+        };
+
+        var mappedMovie = TinyMapper.Map<Movie>(movieUpdateRequest);
+
+        Assert.Equal(movieUpdateRequest.Title, mappedMovie.Title);
+        Assert.Equal(movieUpdateRequest.Synopsis, mappedMovie.Synopsis);
+        Assert.Equal(movieUpdateRequest.VideoSource, mappedMovie.VideoSource);
+        Assert.Equal(movieUpdateRequest.ReleaseYear, mappedMovie.ReleaseYear);
+        Assert.Equal(movieUpdateRequest.DurationInMinutes, mappedMovie.DurationInMinutes);
+    }
 }
