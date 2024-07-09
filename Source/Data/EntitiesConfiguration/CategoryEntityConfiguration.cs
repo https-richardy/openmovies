28e1be2d0eb1b9792131c6fe00301145ba6d05c2
https-richardy <code.richardy@gmail.com>
2024-07-09 02:14:29 -0300

    added [ExcludeFromCodeCoverage] attribute to ignore method coverage analysis.

--- Source/Data/EntitiesConfiguration/CategoryEntityConfiguration.cs
+++ Source/Data/EntitiesConfiguration/CategoryEntityConfiguration.cs
@@ -2,6 +2,7 @@ namespace OpenMovies.WebApi.Data.Configuration;
 
 public sealed class CategoryEntityConfiguration : IEntityTypeConfiguration<Category>
 {
+    [ExcludeFromCodeCoverage]
     public void Configure(EntityTypeBuilder<Category> builder)
     {
         builder.ToTable("Categories");
