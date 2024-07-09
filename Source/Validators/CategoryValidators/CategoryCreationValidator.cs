c70a4d9a6cd4f51259367447d2a0662c307830db
https-richardy <code.richardy@gmail.com>
2024-07-09 10:44:35 -0300

    added category creation request validator.

---
+++ Source/Validators/CategoryValidators/CategoryCreationValidator.cs
@@ -0,0 +1,12 @@
+namespace OpenMovies.WebApi.Validators;
+
+public sealed class CategoryCreationValidator : AbstractValidator<CategoryCreationRequest>
+{
+    public CategoryCreationValidator()
+    {
+        RuleFor(category => category.Name)
+            .NotEmpty().WithMessage("Category name is required.")
+            .MinimumLength(3).WithMessage("Category name must be at least 3 characters.")
+            .MaximumLength(50).WithMessage("Category name must be at most 50 characters.");
+    }
+}
