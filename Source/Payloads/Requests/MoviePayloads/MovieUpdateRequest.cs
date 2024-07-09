67f76adfc9cd605d243b15ca76b985b399a87202
https-richardy <code.richardy@gmail.com>
2024-07-09 13:27:12 -0300

    property access modifier updated to settable.

--- Source/Payloads/Requests/MoviePayloads/MovieUpdateRequest.cs
+++ Source/Payloads/Requests/MoviePayloads/MovieUpdateRequest.cs
@@ -9,7 +9,7 @@ public sealed record MovieUpdateRequest : IRequest<Response>
     public int ReleaseYear { get; init; }
     public int DurationInMinutes { get; init; }
     public int CategoryId { get; init; }
-    public int MovieId { get; init; }
+    public int MovieId { get; set; }
 
     public IFormFile Image { get; init; }
 }
