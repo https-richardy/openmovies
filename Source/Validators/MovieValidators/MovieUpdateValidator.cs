4bc4787336635212291936e86ca80c1fe357d1bf
https-richardy <code.richardy@gmail.com>
2024-07-09 12:25:27 -0300

    added movie update request validator.

---
+++ Source/Validators/MovieValidators/MovieUpdateValidator.cs
@@ -0,0 +1,31 @@
+namespace OpenMovies.WebApi.Validators;
+
+public sealed class MovieUpdateValidator :
+    AbstractValidator<MovieUpdateRequest>, IValidator<MovieUpdateRequest>
+{
+    public MovieUpdateValidator()
+    {
+        RuleFor(movie => movie.Title)
+            .NotEmpty().WithMessage("Movie title is required.")
+            .MaximumLength(120).WithMessage("Movie title must be at most 120 characters.");
+    
+        RuleFor(movie => movie.Synopsis)
+            .NotEmpty().WithMessage("Movie synopsis is required.")
+            .MaximumLength(1000).WithMessage("Movie synopsis must be at most 1000 characters.");
+
+        RuleFor(movie => movie.VideoSource)
+            .NotEmpty().WithMessage("Movie video source (CDN link) is required.");
+
+        RuleFor(movie => movie.ReleaseYear)
+            .NotEmpty().WithMessage("Movie release year is required.")
+            .LessThanOrEqualTo(DateTime.Now.Year).WithMessage("Movie release year must be in the past.");
+
+        RuleFor(movie => movie.DurationInMinutes)
+            .NotEmpty().WithMessage("Movie duration in minutes is required.")
+            .GreaterThan(0).WithMessage("Movie duration in minutes must be greater than 0.");
+
+        RuleFor(movie => movie.CategoryId)
+            .NotEmpty().WithMessage("Movie category is required.")
+            .GreaterThan(0).WithMessage("Movie category must be greater than 0.");
+    }
+}
