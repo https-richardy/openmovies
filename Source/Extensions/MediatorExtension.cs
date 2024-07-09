2bf5eabd10e901f1a746d2628ffecfc9a2cc7391
https-richardy <code.richardy@gmail.com>
2024-07-09 16:27:17 -0300

    request handler for getting movie details was registered.

--- Source/Extensions/MediatorExtension.cs
+++ Source/Extensions/MediatorExtension.cs
@@ -20,6 +20,7 @@ public static class MediatorExtension
         #region handlers for movies
 
         services.AddScoped<IRequestHandler<MovieRetrievalRequest, Response<PaginationHelper<Movie>>>, MovieRetrievalHandler>();
+        services.AddScoped<IRequestHandler<MovieDetailsRequest, Response<Movie>>, MovieDetailsHandler>();
         services.AddScoped<IRequestHandler<MovieCreationRequest, Response>, MovieCreationHandler>();
         services.AddScoped<IRequestHandler<MovieUpdateRequest, Response>, MovieUpdateHandler>();
 
