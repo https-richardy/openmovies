de8f0621829b20ee5e5ad40600b0735e5eb97be7
https-richardy <code.richardy@gmail.com>
2024-07-08 21:22:32 -0300

    updated movie repository to provide logger.

--- Source/Data/Repositories/MovieRepository.cs
+++ Source/Data/Repositories/MovieRepository.cs
@@ -1,7 +1,7 @@
 namespace OpenMovies.WebApi.Data.Repositories;
 
-public sealed class MovieRepository(AppDbContext dbContext) :
-    Repository<Movie, AppDbContext>(dbContext), IMovieRepository
+public sealed class MovieRepository(AppDbContext dbContext, ILogger<MovieRepository> logger) :
+    Repository<Movie, AppDbContext>(dbContext, logger), IMovieRepository
 {
-    
+
 }
