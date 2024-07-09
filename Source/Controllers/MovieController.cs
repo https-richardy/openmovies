1bda0076e7a2f1327c0a9b4ef54bc5f1fe527b08
https-richardy <code.richardy@gmail.com>
2024-07-09 01:53:07 -0300

    added movie controller.

---
+++ Source/Controllers/MovieController.cs
@@ -0,0 +1,15 @@
+namespace OpenMovies.WebApi.Controllers;
+
+[ApiController]
+[Route("api/movies")]
+public sealed class MovieController(IMediator mediator) : ControllerBase
+{
+
+    [HttpPost]
+    [Authorize(Roles = "Admin")]
+    public async Task<IActionResult> CreateMovieAsync(MovieCreationRequest request)
+    {
+        var response = await mediator.Send(request);
+        return StatusCode(response.StatusCode, response);
+    }
+}
