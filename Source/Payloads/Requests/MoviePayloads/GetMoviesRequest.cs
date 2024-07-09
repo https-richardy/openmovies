d57ff4ee6deb070832d3c3dbfa7d5b0115564d3a
https-richardy <code.richardy@gmail.com>
2024-07-09 14:50:12 -0300

    update GetMoviesRequest to use Response for standardized API responses

--- Source/Payloads/Requests/MoviePayloads/GetMoviesRequest.cs
+++ Source/Payloads/Requests/MoviePayloads/GetMoviesRequest.cs
@@ -1,6 +1,6 @@
 namespace OpenMovies.WebApi.Payloads;
 
-public sealed record GetMoviesRequest : IRequest<PaginationHelper<Movie>>
+public sealed record GetMoviesRequest : IRequest<Response<PaginationHelper<Movie>>>
 {
     public int Page { get; init; } = 1;
     public int PageSize { get; init; } = 10;
