e28548e70650da180fb51012c14ea18046e6161f
https-richardy <code.richardy@gmail.com>
2024-07-09 12:40:14 -0300

    added mapping settings between a movie update request and a movie entity.

--- Source/Extensions/MappingExtension.cs
+++ Source/Extensions/MappingExtension.cs
@@ -15,6 +15,15 @@ public static class MappingExtension
             config.Bind(target: target => target.DurationInMinutes, source: source => source.DurationInMinutes);
         });
 
+        TinyMapper.Bind<MovieUpdateRequest, Movie>(config =>
+        {
+            config.Bind(target: target => target.Title, source: source => source.Title);
+            config.Bind(target: target => target.Synopsis, source: source => source.Synopsis);
+            config.Bind(target: target => target.VideoSource, source: source => source.VideoSource);
+            config.Bind(target: target => target.ReleaseYear, source: source => source.ReleaseYear);
+            config.Bind(target: target => target.DurationInMinutes, source: source => source.DurationInMinutes);
+        });
+
         #endregion
     }
 }
