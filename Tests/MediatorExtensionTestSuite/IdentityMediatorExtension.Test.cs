68cb2ec991f345d5e792d2aa72a6c920cbd26f0c
https-richardy <code.richardy@gmail.com>
2024-07-09 01:29:41 -0300

    added unit test to verify that identity request handlers are being registered in the ServiceCollection.

---
+++ Tests/MediatorExtensionTestSuite/IdentityMediatorExtension.Test.cs
@@ -0,0 +1,32 @@
+namespace OpenMovies.TestingSuite.MediatorExtensionTestSuite;
+
+public class IdentityMediatorExtensionTest
+{
+    private readonly IServiceCollection _services;
+
+    public IdentityMediatorExtensionTest()
+    {
+        _services = new ServiceCollection();
+        _services.AddMediator();
+    }
+
+    [Fact(DisplayName = "Verify if AccountRegistrationHandler is registered.")]
+    public void VerifyIfAccountRegistrationHandlerIsRegistered()
+    {
+        var handler = _services.BuildServiceProvider()
+            .GetRequiredService<IRequestHandler<AccountRegistrationRequest, Response>>();
+
+        Assert.NotNull(handler);
+        Assert.IsType<AccountRegistrationHandler>(handler);
+    }
+
+    [Fact(DisplayName = "Verify if AuthenticationHandler is registered.")]
+    public void VerifyIfAuthenticationHandlerIsRegistered()
+    {
+        var handler = _services.BuildServiceProvider()
+            .GetRequiredService<IRequestHandler<AuthenticationCredentials, Response<AuthenticationResponse>>>();
+
+        Assert.NotNull(handler);
+        Assert.IsType<AuthenticationHandler>(handler);
+    }
+}
