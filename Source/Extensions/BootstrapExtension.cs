93a5122b35fb682e4291ebd0cc7ff3abe1231850
https-richardy <code.richardy@gmail.com>
2024-07-09 01:59:17 -0300

    removed any reference from super admin to just admin.

--- Source/Extensions/BootstrapExtension.cs
+++ Source/Extensions/BootstrapExtension.cs
@@ -5,9 +5,9 @@ namespace OpenMovies.WebApi.Extensions;
 /*
    BootstrapExtension serves the purpose of initializing essential configurations
    during the application startup. Specifically, it ensures the existence of a
-   super-administrator user role and creates a super-admin user if one doesn't exist.
+   administrator user role and creates a admin user if one doesn't exist.
    This is crucial for setting up initial access control and administrative privileges
-   before any other operations can proceed. Similar to Django's create super admin feature,
+   before any other operations can proceed. Similar to Django's create admin feature,
    this extension ensures that the application starts with necessary administrative
    capabilities securely in place.
 */
@@ -23,25 +23,25 @@ public static class BootstrapExtension
             var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
             var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 
-            var superAdminRoleExists = await roleManager.RoleExistsAsync("SuperAdmin");
-            if (!superAdminRoleExists)
+            var adminRoleExists = await roleManager.RoleExistsAsync("Administrator");
+            if (!adminRoleExists)
             {
-                var superAdminRole = new IdentityRole("SuperAdmin");
-                await roleManager.CreateAsync(superAdminRole);
+                var adminRole = new IdentityRole("Administrator");
+                await roleManager.CreateAsync(adminRole);
             }
 
             IdentityResult result;
 
             do
             {
-                var usersInRole = await userManager.GetUsersInRoleAsync("SuperAdmin");
+                var usersInRole = await userManager.GetUsersInRoleAsync("Administrator");
                 if (usersInRole.Count > 0)
                 {
                     break;
                 }
 
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("super-admin user not found. Let's create a new one.");
+                Console.WriteLine("admin user not found. Let's create a new one.");
                 Console.ResetColor();
 
                 Console.Write("Enter an username: ");
@@ -53,26 +53,26 @@ public static class BootstrapExtension
                 Console.Write("Enter a password: ");
                 var password = Console.ReadLine();
 
-                var superAdminUser = new IdentityUser
+                var adminUser = new IdentityUser
                 {
                     UserName = username,
                     Email = email,
                 };
 
-                result = await userManager.CreateAsync(superAdminUser, password);
+                result = await userManager.CreateAsync(adminUser, password);
 
                 if (result.Succeeded)
                 {
-                    await userManager.AddToRoleAsync(superAdminUser, "SuperAdmin");
+                    await userManager.AddToRoleAsync(adminUser, "Administrator");
 
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"super-admin ({email}) user successfully created.");
+                    Console.WriteLine($"administrator ({username} - {email}) user successfully created.");
                     Console.ResetColor();
                 }
                 else
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Error creating super-admin user:");
+                    Console.WriteLine("Error creating admin user:");
 
                     foreach (var error in result.Errors)
                     {
