dbcfba94394f9d9fd0b274bcbc8064310c842031
https-richardy <code.richardy@gmail.com>
2024-07-11 19:44:35 -0300

    updated identity type configuration.

--- Source/Extensions/IdentityExtension.cs
+++ Source/Extensions/IdentityExtension.cs
@@ -5,7 +5,7 @@ public static class IdentityExtension
     [ExcludeFromCodeCoverage]
     public static void ConfigureIdentity(this IServiceCollection services)
     {
-        services.AddIdentity<IdentityUser, IdentityRole>(options =>
+        services.AddIdentity<ApplicationUser, IdentityRole>(options =>
         {
             options.User.RequireUniqueEmail = true;
             options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ";
