b59b9dbd9d38d910526a8dc5cdad7875773b77bd
https-richardy <code.richardy@gmail.com>
2024-07-11 17:30:27 -0300

    added global usage directive for the namespace: OpenMovies.WebApi.Identity.

--- Source/Usings.cs
+++ Source/Usings.cs
@@ -27,6 +27,7 @@ global using OpenMovies.WebApi.Data;
 global using OpenMovies.WebApi.Data.Repositories;
 global using OpenMovies.WebApi.Extensions;
 global using OpenMovies.WebApi.Entities;
+global using OpenMovies.WebApi.Identity;
 global using OpenMovies.WebApi.Services;
 global using OpenMovies.WebApi.Services.Exceptions;
 global using OpenMovies.WebApi.Payloads;
