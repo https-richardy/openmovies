b3c6a25dc1234af90085a5e06305207dd2a18219
https-richardy <code.richardy@gmail.com>
2024-07-11 17:32:35 -0300

    added a new Profile class to represent an account profile.

---
+++ Source/Entities/Profile.cs
@@ -0,0 +1,20 @@
+namespace OpenMovies.WebApi.Entities;
+
+public sealed class Profile
+{
+    public string Name { get; set; }
+    public bool IsChild { get; set; }
+
+    public ApplicationUser Account { get; set; }
+
+    public ICollection<BookmarkedMovie> BookmarkedMovies { get; set; }
+    public ICollection<WatchedMovie> WatchedMovies { get; set; }
+
+    public Profile()
+    {
+        /*
+            Default parameterless constructor included due to Entity Framework Core not setting navigation properties
+            when using constructors. For more information, see: https://learn.microsoft.com/pt-br/ef/core/modeling/constructors
+        */
+    }
+}
