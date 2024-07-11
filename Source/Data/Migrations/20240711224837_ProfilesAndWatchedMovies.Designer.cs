e8ec801973f9bef6cc6db06204a28dc88bf04e5e
https-richardy <code.richardy@gmail.com>
2024-07-11 20:28:20 -0300

    added new migrations: New tables, 'profile' and 'movies watched'.

--- Source/Data/Migrations/20240711224837_ProfilesAndWatchedMovies.Designer.cs
+++ Source/Data/Migrations/20240711224837_ProfilesAndWatchedMovies.Designer.cs
@@ -2,17 +2,20 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OpenMovies.WebApi.Data;
 
 #nullable disable
 
-namespace OpenMovies.Data.Migrations
+namespace OpenMovies.WebApi.Data.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240711224837_ProfilesAndWatchedMovies")]
+    partial class ProfilesAndWatchedMovies
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.0");
