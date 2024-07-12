2ee99d093e3764775675eec5a21bd6aa3419fae6
https-richardy <code.richardy@gmail.com>
2024-07-12 08:33:34 -0300

    updated profile repository interface. Due to SRP violation found.

--- Source/Data/Repositories/Interfaces/IProfileRepository.cs
+++ Source/Data/Repositories/Interfaces/IProfileRepository.cs
@@ -2,9 +2,6 @@ namespace OpenMovies.WebApi.Data.Repositories;
 
 public interface IProfileRepository : IRepository<Profile>
 {
-    Task<IEnumerable<Profile>> GetUserProfilesAsync(ApplicationUser user);
     Task<Profile?> GetUserProfileByIdAsync(ApplicationUser user, int profileId);
-
-    Task<IEnumerable<BookmarkedMovie>> GetBookmarkedMoviesAsync(Profile profile);
-    Task<IEnumerable<WatchedMovie>> GetWatchedMoviesAsync(Profile profile);
+    Task<IEnumerable<Profile>> GetUserProfilesAsync(ApplicationUser user);
 }
