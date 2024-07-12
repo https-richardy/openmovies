749e0069204847fb3e7ac0dbd66df1bb72fb57ab
https-richardy <code.richardy@gmail.com>
2024-07-12 10:12:21 -0300

    user identifier type updated to string.

--- Source/Exceptions/MaxProfileCountReachedException.cs
+++ Source/Exceptions/MaxProfileCountReachedException.cs
@@ -2,7 +2,7 @@ namespace OpenMovies.WebApi.Exceptions;
 
 public sealed class MaxProfileCountReachedException : Exception
 {
-    public MaxProfileCountReachedException(int userId, int maxNumberOfProfiles)
+    public MaxProfileCountReachedException(string userId, int maxNumberOfProfiles)
         : base($"User with ID: {userId} has reached the maximum number of profiles allowed ({maxNumberOfProfiles}).")
     {
         
