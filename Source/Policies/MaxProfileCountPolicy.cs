f2443e2f498ffcc8fcc369e17cddc101948851ae
https-richardy <code.richardy@gmail.com>
2024-07-12 09:57:55 -0300

    added XML docs.

--- Source/Policies/MaxProfileCountPolicy.cs
+++ Source/Policies/MaxProfileCountPolicy.cs
@@ -1,9 +1,24 @@
 namespace OpenMovies.WebApi.Policies;
 
+/// <summary>
+/// Implements the <see cref="IProfileCreationPolicy"/> interface to enforce a maximum number of profiles per user.
+/// </summary>
+/// <remarks>
+/// This policy checks if a user has reached the maximum allowed number of profiles before allowing them to create a new one.
+/// The maximum number of profiles is defined by the private constant <c>_maxNumberOfProfilesPerAccount</c>.
+/// </remarks>
 public sealed class MaxProfileCountPolicy(UserManager<ApplicationUser> userManager) : IProfileCreationPolicy
 {
     private const int _maxNumberOfProfilesPerAccount = 4;
 
+    /// <summary>
+    /// Asynchronously checks if a user can create a new profile based on the maximum profile count.
+    /// </summary>
+    /// <param name="userId">The ID of the user requesting to create a profile.</param>
+    /// <returns>
+    /// A task that resolves to true if the user can create a profile (i.e., hasn't reached the maximum limit), 
+    /// false otherwise.
+    /// </returns>
     public async Task<bool> CanCreateProfileAsync(string userId)
     {
         var user = await userManager.FindByIdAsync(userId);
