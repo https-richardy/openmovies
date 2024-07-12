8dcc78d770e64e523e16b526a88f235aefb7ffa8
https-richardy <code.richardy@gmail.com>
2024-07-12 08:35:38 -0300

    ProfileRepository class refactored due to SRP violation

--- Source/Data/Repositories/ProfileRepository.cs
+++ Source/Data/Repositories/ProfileRepository.cs
@@ -5,18 +5,6 @@ public sealed class ProfileRepository(
     ILogger<ProfileRepository> logger
 ) : Repository<Profile, AppDbContext>(dbContext, logger), IProfileRepository
 {
-    public async Task<IEnumerable<BookmarkedMovie>> GetBookmarkedMoviesAsync(Profile profile)
-    {
-        Logger.LogInformation("Fetching for movies bookmarked to the profile: {profileId}", profile.Id);
-
-        var bookmarkedMovies = await DbContext.BookmarkedMovies
-            .Include(bookmarkedMovie => bookmarkedMovie.Profile)
-            .Where(bookmarkedMovie => bookmarkedMovie.Profile.Id == profile.Id)
-            .ToListAsync();
-
-        return bookmarkedMovies;
-    }
-
     public async Task<Profile?> GetUserProfileByIdAsync(ApplicationUser user, int profileId)
     {
         Logger.LogInformation("Fetching profile: {profileId} for user: {userId}", profileId, user.Id);
@@ -37,16 +25,5 @@ public sealed class ProfileRepository(
             .ToListAsync();
 
         return profile;
-    }
-
-    public async Task<IEnumerable<WatchedMovie>> GetWatchedMoviesAsync(Profile profile)
-    {
-        Logger.LogInformation("Fetching watched movies for profile: {userId}", profile.Id);
-
-        var watchedMovies = await DbContext.WatchedMovies
-            .Where(watchedMovie => watchedMovie.Profile.Id == profile.Id)
-            .ToListAsync();
-
-        return watchedMovies;
     }
 }
