92e1eafcdd2ad0181d8b549dba06a8e957af0aa2
https-richardy <code.richardy@gmail.com>
2024-07-13 15:55:20 -0300

    added unit tests to check the behavior of the UserContextService.

---
+++ Tests/ServicesTestSuite/UserContextService.Test.cs
@@ -0,0 +1,73 @@
+namespace OpenMovies.TestingSuite.ServicesTestSuite;
+
+public sealed class UserContextServiceTest
+{
+    private readonly Mock<IHttpContextAccessor> _contextAccessorMock;
+    private readonly Mock<ILogger<UserContextService>> _loggerMock;
+    private readonly IUserContextService _userContextService;
+
+    public UserContextServiceTest()
+    {
+        _contextAccessorMock = new Mock<IHttpContextAccessor>();
+        _loggerMock = new Mock<ILogger<UserContextService>>();
+
+        _userContextService = new UserContextService(
+            _contextAccessorMock.Object,
+            _loggerMock.Object
+        );
+    }
+
+    [Fact(DisplayName = "UserContextService: Should get user id from http context")]
+    public void GetCurrentUserId_UserInContext_ReturnsUserId()
+    {
+        var userId = Guid.NewGuid().ToString();
+        var claims = new List<Claim> { new Claim(ClaimTypes.NameIdentifier, userId) };
+        var identity = new ClaimsIdentity(claims, "TestAuthType");
+
+        var claimsPrincipal = new ClaimsPrincipal(identity);
+        var context = new DefaultHttpContext { User = claimsPrincipal };
+
+        _contextAccessorMock.Setup(accessor => accessor.HttpContext)
+            .Returns(context);
+
+        var result = _userContextService.GetCurrentUserId();
+        Assert.Equal(userId, result);
+    }
+
+    [Fact()]
+    public void GetCurrentUserId_NoUserInContext_ReturnsNull()
+    {
+        #pragma warning disable CS8600 // this needs to be null and void in this scenario.
+        _contextAccessorMock.Setup(accessor => accessor.HttpContext)
+            .Returns((HttpContext)null);
+
+        var result = _userContextService.GetCurrentUserId();
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public void GetCurrentUserClaimsPrincipal_UserInContext_ReturnsClaimsPrincipal()
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.Name, "John Doe"),
+            new Claim(ClaimTypes.Role, "Admin")
+        };
+
+        var identity = new ClaimsIdentity(claims, "TestAuthType");
+        var claimsPrincipal = new ClaimsPrincipal(identity);
+        var context = new DefaultHttpContext { User = claimsPrincipal };
+
+        _contextAccessorMock.Setup(accessor => accessor.HttpContext)
+            .Returns(context);
+
+        var result = _userContextService.GetCurrentUserClaimsPrincipal();
+
+        Assert.Equal(claimsPrincipal, result);
+
+        Assert.Equal(claimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier), result?.FindFirstValue(ClaimTypes.NameIdentifier));
+        Assert.Equal(claimsPrincipal.FindFirstValue(ClaimTypes.Name), result?.FindFirstValue(ClaimTypes.Name));
+        Assert.Equal(claimsPrincipal.FindFirstValue(ClaimTypes.Role), result?.FindFirstValue(ClaimTypes.Role));
+    }
+}
