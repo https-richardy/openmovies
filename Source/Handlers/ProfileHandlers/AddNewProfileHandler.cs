62828c0ddb6718866e39849d0f6c3ded6e470ced
https-richardy <code.richardy@gmail.com>
2024-07-13 15:14:43 -0300

    added a new handler to handle the request to create a new user profile.

---
+++ Source/Handlers/ProfileHandlers/AddNewProfileHandler.cs
@@ -0,0 +1,55 @@
+namespace OpenMovies.WebApi.Handlers;
+
+public sealed class AddNewProfileHandler(
+    UserManager<ApplicationUser> userManager,
+    IUserContextService userContextService,
+    IProfileManager profileManager,
+    IFileUploadService fileUploadService
+) : IRequestHandler<AddNewProfileRequest, Response>
+{
+    #pragma warning disable CS8604
+    public async Task<Response> Handle(
+        AddNewProfileRequest request, 
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            var userId = userContextService.GetCurrentUserId();
+            var user = await userManager.FindByIdAsync(userId);
+
+            if (user is null)
+                return new Response(
+                    statusCode: StatusCodes.Status404NotFound,
+                    message: "User not found."
+                );
+
+            var profile = new Profile
+            {
+                Name = request.Name,
+                Account = user
+            };
+
+            if (request.Avatar != null)
+            {
+                var imagePath = await fileUploadService.UploadFileAsync(request.Avatar);
+                profile.Avatar = imagePath;
+            }
+
+            await profileManager.SaveUserProfileAsync(user.Id, profile);
+            return new Response
+            {
+                StatusCode = StatusCodes.Status201Created,
+                Message = "Profile created successfully."
+            };
+
+        }
+        catch (MaxProfileCountReachedException exception)
+        {
+            return new Response
+            {
+                StatusCode = StatusCodes.Status403Forbidden,
+                Message = exception.Message
+            };
+        }
+    }
+}
