7a5f385f3af7d0c00f975450870a26f6037142fa
https-richardy <code.richardy@gmail.com>
2024-07-13 16:46:28 -0300

    added record representing the payload of a profile edit request.

---
+++ Source/Payloads/Requests/ProfilePayloads/ProfileEditingRequest.cs
@@ -0,0 +1,8 @@
+namespace OpenMovies.WebApi.Payloads;
+
+public sealed record ProfileEditingRequest : AuthenticatedRequest, IRequest<Response>
+{
+    public string Name { get; init; }
+    public bool IsChild { get; init; }
+    public IFormFile? Avatar { get; init; }
+}
