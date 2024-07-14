70451cba10d7bbbf62c24ec8c9639d87c54aac01
https-richardy <code.richardy@gmail.com>
2024-07-13 22:28:50 -0300

    added unit test to verify that the mapping configuration correctly maps a Profile to a ProfileInformation correctly.

--- Tests/MappingTestSuite/ProfileMapping.Test.cs
+++ Tests/MappingTestSuite/ProfileMapping.Test.cs
@@ -39,6 +39,23 @@ public sealed class ProfileMappingTest
 
         Assert.Equal(profileUpdateRequest.Name, mappedProfile.Name);
         Assert.Equal(profileUpdateRequest.IsChild, mappedProfile.IsChild);
-        Assert.Null(mappedProfile.Avatar); // Assuming Avatar is ignored in mapping for update as well
+        Assert.Null(mappedProfile.Avatar); // Since Avatar is ignored in mapping for update as well
+    }
+
+    [Fact(DisplayName = "Should map a Profile entity to a ProfileInformation correctly.")]
+    public void ShouldMapProfileToProfileInformationCorrectly()
+    {
+        var profile = new Profile
+        {
+            Id = 2005,
+            Name = "John Doe",
+            Avatar = "avatar-url"
+        };
+
+        var profileInformation = TinyMapper.Map<ProfileInformation>(profile);
+
+        Assert.Equal(profile.Id, profileInformation.Id);
+        Assert.Equal(profile.Name, profileInformation.DisplayName);
+        Assert.Equal(profile.Avatar, profileInformation.Avatar);
     }
 }
