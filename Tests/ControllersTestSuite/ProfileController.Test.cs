2dd4a08c9ce2739c27480bd8e8de2e773e311584
https-richardy <code.richardy@gmail.com>
2024-07-13 22:20:47 -0300

    added unit test to check the behavior of the GetUserProfilesAsync method of the Profile Controller.

--- Tests/ControllersTestSuite/ProfileController.Test.cs
+++ Tests/ControllersTestSuite/ProfileController.Test.cs
@@ -84,37 +84,34 @@ public sealed class ProfileControllerTest
         Assert.Equal("Profile created successfully.", objectResultValue.Message);
     }
 
-    [Fact(DisplayName = "Dado uma exceção de MaxProfileCountReachedException, deve retornar resposta de 403 Forbidden")]
-    public async Task GivenMaxProfileCountReachedException_ShouldReturnForbiddenResponse()
+    [Fact(DisplayName = "Given a valid request, should return 200 OK with profiles")]
+    public async Task GivenValidRequest_ShouldReturnOkWithProfiles()
     {
-        var request = _fixture.Build<ProfileCreationRequest>()
-            .With(request => request.Avatar, new Mock<IFormFile>().Object)
+        var profiles = _fixture
+            .CreateMany<ProfileInformation>(3)
+            .ToList();
+
+        var response = _fixture.Build<Response<IEnumerable<ProfileInformation>>>()
+            .With(request => request.StatusCode, StatusCodes.Status200OK)
+            .With(request => request.Data, profiles)
+            .With(request => request.Message, "Profiles successfully recovered.")
             .Create();
 
-        var userId = Guid.NewGuid().ToString();
-        var expectedResponse = new Response
-        {
-            StatusCode = StatusCodes.Status403Forbidden,
-            Message = "Maximum profile count reached."
-        };
+        _mediatorMock.Setup(mediator => mediator.Send(It.IsAny<ProfilesRetrievalRequest>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(response);
 
-        _mediatorMock
-            .Setup(mediator => mediator.Send(request, default))
-            .ReturnsAsync(expectedResponse);
+        var result = await _controller.GetUserProfilesAsync();
 
-        var response = await _controller.CreateProfileAsync(request);
-        var objectResult = response as ObjectResult;
-        var objectResultValue = objectResult?.Value as Response;
+        var objectResult = Assert.IsType<ObjectResult>(result);
+        var actualResponse = Assert.IsType<Response<IEnumerable<ProfileInformation>>>(objectResult.Value);
 
-        _mediatorMock.Verify(mediator => mediator.Send(request, default), Times.Once);
+        Assert.Equal(StatusCodes.Status200OK, objectResult.StatusCode);
 
-        Assert.NotNull(response);
-        Assert.IsType<ObjectResult>(response);
+        Assert.NotNull(actualResponse);
+        Assert.NotNull(actualResponse.Data);
 
-        Assert.NotNull(objectResult);
-        Assert.Equal(expectedResponse.StatusCode, objectResult.StatusCode);
+        Assert.Equal(expected: objectResult.Value, actual: actualResponse);
 
-        Assert.NotNull(objectResultValue);
-        Assert.Equal(expectedResponse.StatusCode, objectResultValue.StatusCode);
+        _mediatorMock.Verify(mediator => mediator.Send(It.IsAny<ProfilesRetrievalRequest>(), It.IsAny<CancellationToken>()), Times.Once);
     }
 }
