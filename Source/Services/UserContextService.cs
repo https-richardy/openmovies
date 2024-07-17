ab52b502323eb5b46a1452e669226862eb0b5d22
https-richardy <code.richardy@gmail.com>
2024-07-17 13:27:23 -0300

    implemented the method to retrieve the current user ID.

--- Source/Services/UserContextService.cs
+++ Source/Services/UserContextService.cs
@@ -5,6 +5,9 @@ public sealed class UserContextService(
     ILogger<UserContextService> logger
 ) : IUserContextService
 {
+    private readonly IHttpContextAccessor _contextAccessor = contextAccessor;
+    private readonly ILogger<UserContextService> _logger = logger;
+
     public string? GetCurrentUserId()
     {
         var currentUserId = GetCurrentUserClaimsPrincipal()?.FindFirstValue(ClaimTypes.NameIdentifier);
@@ -13,20 +16,36 @@ public sealed class UserContextService(
 
     public ClaimsPrincipal? GetCurrentUserClaimsPrincipal()
     {
-        var claimsPrincipal = contextAccessor.HttpContext?.User;
+        var claimsPrincipal = _contextAccessor.HttpContext?.User;
 
         var currentUserId = claimsPrincipal?.FindFirstValue(ClaimTypes.NameIdentifier);
         var currentName = claimsPrincipal?.FindFirstValue(ClaimTypes.Name);
         var currentRole = claimsPrincipal?.FindFirstValue(ClaimTypes.Role);
 
         if (claimsPrincipal == null)
-            logger.LogWarning("No user found in the context.");
+            _logger.LogWarning("No user found in the context.");
 
-        logger.LogInformation(
+        _logger.LogInformation(
             "Current user information: Id: {CurrentUserId}, Name: {CurrentName}, Role: {CurrentUserRole}",
             currentUserId, currentName, currentRole
         );
 
-        return contextAccessor.HttpContext?.User;
+        return _contextAccessor.HttpContext?.User;
+    }
+
+    public int GetCurrentActiveProfileId()
+    {
+        var claimsPrincipal = _contextAccessor.HttpContext?.User;
+
+        var currentName = claimsPrincipal?.FindFirstValue(ClaimTypes.Name);
+        var currentActiveProfileId = claimsPrincipal?.FindFirstValue(CustomClaimTypes.ActiveProfileIdentifier);
+        var currentActiveProfileName = claimsPrincipal?.FindFirstValue(CustomClaimTypes.ActiveProfileName);
+
+        _logger.LogInformation(
+            "Current user information: Name: {CurrentName}, Active Profile Id: {CurrentActiveProfileId}, Active Profile Name: {CurrentActiveProfileName}",
+            currentName, currentActiveProfileId, currentActiveProfileName
+        );
+
+        return int.TryParse(currentActiveProfileId, out var profileId) ? profileId : 0;
     }
 }
